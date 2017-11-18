991bb5fcd4adcf4e7d15b70e8dbaffdd76884769
Tristan Bull <tbull@Tristans-MacBook-Pro-2.local>
2017-11-17 18:39:27 -0800

    flask server up and running

--- blockchain.py
+++ blockchain.py
@@ -3,6 +3,9 @@ import json
 from time import time
 from uuid import uuid4
 
+from flask import Flask
+from flask import jsonify
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
@@ -52,7 +55,7 @@ class Blockchain(object):
 
     @staticmethod
     def hash(block):
-       """
+        """
         Creates a SHA-256 hash of a Block
         :param block: <dict> Block
         :return: <str>
@@ -93,3 +96,32 @@ class Blockchain(object):
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
+
+
+# FLASK SHIZ
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new Block"
+  
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return "We'll add a new transaction"
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)    
